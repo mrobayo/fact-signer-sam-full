e87ef899f58f281a5d0bea22fb58d93034012af6
Mario Robayo <mrobayo@gmail.com>
2023-08-24 23:29:26 -0500

    Removed react-admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,11 @@
 import Dashboard from "./layout/Dashboard/Dashboard.tsx";
 import FacturasPage from "./pages/FacturasPage/FacturasPage.tsx";
-import {CoreAdmin} from "./core";
-import {DataProvider} from "./types.ts";
-
-const defaultDataProvider: DataProvider = {
-    // @ts-ignore
-    create: () => Promise.resolve({ data: { id: 0 } }),
-    // @ts-ignore
-    delete: () => Promise.resolve({ data: {} }),
-    deleteMany: () => Promise.resolve({}),
-    getList: () => Promise.resolve({ data: [], total: 0 }),
-    getMany: () => Promise.resolve({ data: [] }),
-    getManyReference: () => Promise.resolve({ data: [], total: 0 }),
-    // @ts-ignore
-    getOne: () => Promise.resolve({ data: {} }),
-    // @ts-ignore
-    update: () => Promise.resolve({ data: {} }),
-    updateMany: () => Promise.resolve({}),
-};
 
 function App() {
-
-
-
   return (
-    <CoreAdmin dataProvider={defaultDataProvider}>
       <Dashboard>
         <FacturasPage />
       </Dashboard>
-    </CoreAdmin>
   )
 }
 
