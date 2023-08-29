984f66d68ef6a213b6cb06e8edaad8f6e0029474
Mario Robayo <mrobayo@gmail.com>
2023-08-29 02:21:28 -0500

    init spring boot app

---
+++ backend-spring/src/main/java/com/marvic/factsigner/FactsignerApplication.java
@@ -0,0 +1,13 @@
+package com.marvic.factsigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactsignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactsignerApplication.class, args);
+	}
+
+}
