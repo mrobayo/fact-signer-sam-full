bad83d50982730cc906a25d0ecfc05246b8d9cb6
Mario Robayo <mrobayo@gmail.com>
2023-09-11 02:14:56 -0500

    auth

--- frontend/src/services/auth/AuthStatus.tsx
+++ frontend/src/services/auth/AuthStatus.tsx
@@ -3,13 +3,13 @@ import {useAuth} from "./useAuth.ts";
 function AuthStatus() {
   const auth = useAuth();
 
-  if (!auth.user) {
+  if (!auth.user?.login) {
     return <p>You are not logged in.</p>;
   }
 
   return (
     <p>
-      Welcome {auth.user}!{" "}
+      Welcome {auth.user?.login}!{" "}
     </p>
   );
 }
