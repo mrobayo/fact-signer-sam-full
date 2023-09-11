aa31fc116734d46bde0c29463f1bb27cb4a1f222
Mario Robayo <mrobayo@gmail.com>
2023-09-11 11:16:45 -0500

    auth

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,4 +2,4 @@
 export const isDevelopment = import.meta.env.DEV;
 
 export const AppTitle = import.meta.env.VITE_APP_TITLE;
-export const ApiEndpoint = import.meta.env.VITE_APP_API_ENDPOINT;
+export const ApiEndpoint = isDevelopment ? 'http://localhost:8081' : import.meta.env.VITE_APP_API_ENDPOINT;
