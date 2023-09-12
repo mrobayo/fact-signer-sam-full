d3c1b1ed0345aaa0e1b4c2d0ad38d523d2eb1e2f
Mario Robayo <mrobayo@gmail.com>
2023-09-12 01:49:05 -0500

    clientes

--- frontend/src/pages/Clientes/Clientes.tsx
+++ frontend/src/pages/Clientes/Clientes.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import withAuth from "../../services/auth/withAuth.tsx";
 import Box from '@mui/material/Box';
 
@@ -10,44 +10,35 @@ import {
   GridColDef,
   GridPaginationModel,
   GridValueGetterParams,
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridActionsCellItem,
+  GridRowId,
 } from '@mui/x-data-grid';
 import {Title} from "../../components/ui";
 import clienteService from "../../services/clienteService.ts";
 import {getAge} from "../../util";
 import {PageSize} from "../../constants.ts";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import AddIcon from '@mui/icons-material/AddBoxRounded';
 
-const columns: GridColDef[] = [
-  //{ field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'identidad',
-    headerName: 'Ident.',
-    type: 'string',
-    width: 110,
-  },
-  {
-    field: 'name',
-    headerName: 'Nombres Apellidos',
-    width: 190,
-  },
-  {
-    field: 'edad',
-    headerName: 'Edad',
-    width: 60,
-    valueGetter: (params: GridValueGetterParams) => getAge(params.row.birthday)
-  },
-  {
-    field: 'grupoId',
-    headerName: 'Grupo',
-    sortable: false,
-    width: 160,
-  },
-  {
-    field: 'ultimaVenta',
-    headerName: 'Ultima Venta',
-    sortable: false,
-    width: 110,
-  },
-];
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer sx={{ justifyContent: 'flex-end' }}>
+      {/*<GridToolbarColumnsButton />*/}
+      {/*<GridToolbarFilterButton />*/}
+      {/*<GridToolbarDensitySelector />*/}
+      <GridToolbarExport />
+      <span><IconButton
+        color="info"
+        onClick={() => console.log()}>
+        <AddIcon />
+      </IconButton></span>
+    </GridToolbarContainer>
+  );
+}
 
 const Clientes: React.FC = () => {
   const [page, setPage] = useState(0);
@@ -61,11 +52,88 @@ const Clientes: React.FC = () => {
   }, [page]);
   //const apiRef = useGridApiRef();
 
+  const handleEditClick = (id: GridRowId) => () => {
+    console.log('editing', data?.content?.find((row) => row.id === id));
+    //setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  // const handleSaveClick = (id: GridRowId) => () => {
+  //   setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  // };
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    console.log('del', data?.content?.filter((row) => row.id !== id));
+    //setRows();
+  };
+
+  const columns: GridColDef[] = useMemo(() => {
+    return [
+        //{ field: 'id', headerName: 'ID', width: 90 },
+        {
+          field: 'identidad',
+          headerName: 'Ident.',
+          type: 'string',
+          width: 110,
+        },
+        {
+          field: 'name',
+          headerName: 'Nombres Apellidos',
+          flex: 1,
+          minWidth: 200,
+        },
+        {
+          field: 'edad',
+          headerName: 'Edad',
+          width: 60,
+          valueGetter: (params: GridValueGetterParams) => getAge(params.row.birthday)
+        },
+        {
+          field: 'grupoId',
+          headerName: 'Grupo',
+          sortable: false,
+          width: 160,
+        },
+        {
+          field: 'ultimaVenta',
+          headerName: 'Ultima Venta',
+          sortable: false,
+          width: 110,
+        },
+        {
+          field: 'actions',
+          type: 'actions',
+          headerName: '...',
+          width: 100,
+          cellClassName: 'actions',
+          getActions: ({id}) => {
+            //const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+            return [
+              <GridActionsCellItem
+                icon={<EditIcon/>}
+                label="Edit"
+                className="textPrimary"
+                onClick={handleEditClick(id)}
+                color="inherit"
+              />,
+              <GridActionsCellItem
+                icon={<DeleteIcon/>}
+                label="Delete"
+                onClick={handleDeleteClick(id)}
+                color="inherit"
+              />,
+            ];
+          }
+        }
+      ] as GridColDef[];
+  }, []);
+
+
   // if (error) return 'An error has occurred.';
 
   return (
     <div>
       <Title>Clientes</Title>
+
       <Box sx={{ height: 400, width: '100%', backgroundColor: 'white' }}>
         <DataGrid
           //apiRef={apiRef}
@@ -84,26 +152,11 @@ const Clientes: React.FC = () => {
           checkboxSelection
           disableRowSelectionOnClick
           loading={isLoading}
+          slots={{
+            toolbar: CustomToolbar,
+          }}
         />
 
-        {/*<DataGrid*/}
-        {/*  rows={data?.content ?? []}*/}
-        {/*  columns={columns}*/}
-        {/*  // initialState={{*/}
-        {/*  //   pagination: { paginationModel: { pageSize: PageSize, }, },*/}
-        {/*  // }}*/}
-        {/*  pagination*/}
-        {/*  // pageSize={PageSize}*/}
-        {/*  //rowsPerPageOptions={[PageSize]}*/}
-        {/*  rowCount={data?.totalElements ?? 0}*/}
-        {/*  paginationMode="server"*/}
-        {/*  //onPageChange={handlePageChange}*/}
-        {/*  //page={page}*/}
-        {/*  pageSizeOptions={[PageSize]}*/}
-        {/*  checkboxSelection*/}
-        {/*  disableRowSelectionOnClick*/}
-        {/*  loading={isLoading}*/}
-        {/*/>*/}
       </Box>
     </div>
   )
